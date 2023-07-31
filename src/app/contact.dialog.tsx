1b616d849ffbf34483170b9a83e84ea3cd3791d5
Aleksey Vokhmin <vohmina2011@yandex.ru>
2023-07-31 17:33:42 +0300

    Yup resolver instead of Zod Cloudflare turnstile instead of Google ReCAPTCHA New form input

--- src/app/contact.dialog.tsx
+++ src/app/contact.dialog.tsx
@@ -1,8 +1,6 @@
-import { zodResolver } from "@hookform/resolvers/zod";
 import type { DialogProps } from "@radix-ui/react-dialog";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { z } from "zod";
 import SuccessDialog from "./success.dialog";
 import {
   Dialog,
@@ -21,42 +19,49 @@ import {
 import { Input } from "@/components/ui/input";
 import { Checkbox } from "@/components/ui/checkbox";
 import Link from "next/link";
-import ReCAPTCHA from "react-google-recaptcha";
+import { Turnstile } from "@marsidev/react-turnstile";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 export default function ContactDialog({
   ...props
 }: {} & React.PropsWithoutRef<DialogProps>) {
   const [success, setSuccess] = useState(false);
 
-  const formSchema = z.object({
-    name: z.string().min(3, "Name must be at least 3 characters long"),
-    email: z.string().email("Invalid email address"),
-    message: z.string().min(10, "Message must be at least 10 characters long"),
-    personalData: z.boolean().refine((v) => v, {
-      message: "You must agree to our privacy policy",
-    }),
-    captchaToken: z
-      .string()
-      .nullable()
-      .refine((val) => val, "You must solve the captcha"),
+  const formSchema = yup.object().shape({
+    subject: yup.string().required(),
+    name: yup.string().required().min(3),
+    email: yup.string().required().email(),
+    message: yup.string().required().min(10),
+    personalData: yup.boolean().required(),
+    captchaToken: yup.string().required(),
   });
-
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm({
+    resolver: yupResolver(formSchema),
     defaultValues: {
+      subject: "",
       name: "",
       email: "",
       message: "",
       personalData: false,
-      captchaToken: null,
+      captchaToken: undefined,
     },
   });
 
-  function handleSubmit(data: z.infer<typeof formSchema>) {
+  function handleSubmit(data: yup.InferType<typeof formSchema>) {
     console.log(data);
   }
+
+  const subjects = [
+    "Difficulties with registration",
+    "Visa questions",
+    "Website problems",
+    "Want to become a partner?",
+    "Marketing & PR",
+    "Other",
+  ];
 
   return (
     <>
@@ -67,9 +72,9 @@ export default function ContactDialog({
         description="We will get back to you as soon as possible"
       />
       <Dialog {...props}>
-        <DialogContent>
+        <DialogContent className="overflow-y-auto max-h-[80vh] max-w-[85%] sm:max-w-lg">
           <DialogHeader>
-            <DialogTitle className="text-center text-base font-normal">
+            <DialogTitle className=" text-center text-base font-normal">
               To contact the organizing committee please fill in the form below
             </DialogTitle>
           </DialogHeader>
@@ -86,6 +91,30 @@ export default function ContactDialog({
               }}
               className="space-y-4"
             >
+              <FormField
+                control={form.control}
+                name="subject"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <div className="flex gap-2 flex-wrap">
+                        {subjects.map((subject) => (
+                          <Button
+                            key={subject}
+                            onClick={() => field.onChange(subject)}
+                            variant={
+                              field.value === subject ? "default" : "outline"
+                            }
+                          >
+                            {subject}
+                          </Button>
+                        ))}
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
               <FormField
                 control={form.control}
                 name="name"
@@ -130,36 +159,47 @@ export default function ContactDialog({
                 control={form.control}
                 name="personalData"
                 render={({ field }) => (
-                  <FormItem className="flex items-center gap-2">
-                    <FormControl>
-                      <Checkbox
-                        checked={field.value}
-                        onCheckedChange={(value: boolean) =>
-                          field.onChange(value)
-                        }
-                        name={field.name}
-                      />
-                    </FormControl>
-                    <FormLabel className="!m-0">
-                      I agree to the processing of personal data
-                    </FormLabel>
-                    <FormMessage />
+                  <FormItem className="flex flex-col">
+                    <div className="flex items-center gap-1">
+                      <FormControl>
+                        <Checkbox
+                          checked={field.value}
+                          onCheckedChange={(value: boolean) =>
+                            field.onChange(value)
+                          }
+                          className="rounded-full data-[state=checked]:text-primary data-[state=checked]:bg-primary-foreground"
+                          name={field.name}
+                        />
+                      </FormControl>
+                      <FormLabel className="!m-0 hover:cursor-pointer">
+                        I agree to the processing of personal data.
+                      </FormLabel>
+                      <Link
+                        className="text-sm underline text-blue-400"
+                        href="#"
+                      >
+                        Privacy policy
+                      </Link>
+                    </div>
+                    <FormMessage className="!m-0" />
                   </FormItem>
                 )}
               />
-              <Link href="#">Privacy policy</Link>
+
               <FormField
                 control={form.control}
                 name="captchaToken"
                 render={({ field }) => (
                   <FormItem>
                     <FormControl>
-                      <ReCAPTCHA
-                        sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!}
-                        onChange={field.onChange}
-                        theme="dark"
-                        // @ts-expect-error
-                        isolated={true}
+                      <Turnstile
+                        id="contact-turnstile"
+                        siteKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!}
+                        options={{
+                          action: "Contact",
+                          theme: "dark",
+                        }}
+                        onSuccess={(token) => field.onChange(token)}
                       />
                     </FormControl>
                     <FormMessage />
