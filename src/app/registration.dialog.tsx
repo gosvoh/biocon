37645819ef2746483836d9370347c6a3a26d8cf7
Aleksey Vokhmin <vohmina2011@yandex.ru>
2023-07-31 17:34:34 +0300

    Yup resolver instead of Zod Added Cloudflare turnstile Changed form a bit

--- src/app/registration.dialog.tsx
+++ src/app/registration.dialog.tsx
@@ -8,17 +8,14 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import type { DialogProps } from "@radix-ui/react-dialog";
 import type {
   FieldError,
-  Resolver,
   UseFormRegister,
   UseFormSetValue,
 } from "react-hook-form";
 import { useForm } from "react-hook-form";
-import isEmail from "validator/lib/isEmail";
-import isMobilePhone from "validator/lib/isMobilePhone";
 import SuccessDialog from "./success.dialog";
 import {
   Dialog,
@@ -29,6 +26,9 @@ import {
 import { Button } from "@/components/ui/button";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Turnstile } from "@marsidev/react-turnstile";
 
 type RegisterFormValues = {
   name: string;
@@ -46,6 +46,8 @@ type RegisterFormValues = {
   resume: string | undefined;
   scienceProfile: string | undefined;
   video: string | undefined;
+  personalData: boolean;
+  captchaToken: string;
 };
 
 const FormInputField = ({
@@ -80,7 +82,6 @@ const FormSelectField = ({
   label,
   name,
   options,
-  register,
   error,
   placeholder,
   setValue,
@@ -89,7 +90,6 @@ const FormSelectField = ({
   name: keyof RegisterFormValues;
   options: { value: string; label: string }[];
   error?: FieldError;
-  register: UseFormRegister<RegisterFormValues>;
   placeholder?: string;
   setValue: UseFormSetValue<RegisterFormValues>;
 }) => (
@@ -110,6 +110,57 @@ const FormSelectField = ({
     {error && <p className="text-red-500">{error.message}</p>}
   </div>
 );
+
+const FormRadioField = ({
+  label,
+  name,
+  options,
+  error,
+  setValue,
+  value,
+  register,
+}: {
+  label: string;
+  name: keyof RegisterFormValues;
+  options: { value: string; label: string }[];
+  error?: FieldError;
+  setValue: UseFormSetValue<RegisterFormValues>;
+  value?: string;
+  register: UseFormRegister<RegisterFormValues>;
+}) => {
+  const reg = register(name);
+
+  return (
+    <div className="my-4">
+      <Label htmlFor={name}>{label}</Label>
+      <RadioGroup
+        id={name}
+        {...reg}
+        // TODO: changing the value of the radio group doesn't update the form
+        onChange={(e) => {
+          reg.onChange(e);
+        }}
+        onValueChange={(val) => setValue(name, val)}
+      >
+        {options.map((option, i) => (
+          <div key={i} className="flex items-center space-x-2">
+            <RadioGroupItem
+              value={option.value.replace(" ", "_")}
+              id={`${name}-${option.value}`}
+            />
+            <Label
+              className="hover:cursor-pointer"
+              htmlFor={`${name}-${option.value}`}
+            >
+              {option.label}
+            </Label>
+          </div>
+        ))}
+      </RadioGroup>
+      {error && <p className="text-red-500">{error.message}</p>}
+    </div>
+  );
+};
 
 const resolver = yupResolver(
   yup
@@ -162,6 +213,11 @@ const resolver = yupResolver(
           ? schema.required("This field is required")
           : schema;
       }),
+      personalData: yup
+        .boolean()
+        .default(false)
+        .oneOf([true], "This field is required"),
+      captchaToken: yup.string().required("This field is required"),
     })
     .required()
 );
@@ -178,7 +234,9 @@ export default function RegistrationDialog(
     formState: { errors },
     reset: formReset,
     setValue,
+    getValues,
     watch,
+    clearErrors,
   } = useForm<RegisterFormValues>({
     resolver,
   });
@@ -258,8 +316,11 @@ export default function RegistrationDialog(
                 props.onOpenChange?.(false);
                 setSuccess(true);
                 return onSubmit(data);
-              })();
+              }, console.log)();
             }}
+            onChange={() =>
+              console.log("change", getValues("participationType"))
+            }
             className="space-y-4"
           >
             <FormInputField
@@ -267,66 +328,106 @@ export default function RegistrationDialog(
               name="name"
               register={register}
               error={errors.name}
+              placeholder="Enter your full name here"
             />
             <FormInputField
               label="Email"
               name="email"
               register={register}
               error={errors.email}
+              placeholder="Enter your e-mail here"
             />
             <FormInputField
               label="Mobile"
               name="mobile"
               register={register}
               error={errors.mobile}
+              placeholder="Enter your mobile number with country code here"
             />
             <FormInputField
               label="Country"
               name="country"
               register={register}
               error={errors.country}
+              placeholder="---"
             />
             <FormInputField
               label="City"
               name="city"
               register={register}
               error={errors.city}
+              placeholder="---"
             />
             <FormInputField
               label="Affiliation"
               name="affiliation"
               register={register}
               error={errors.affiliation}
+              placeholder="Enter the name of your university here"
             />
             <FormInputField
               label="Role"
               name="role"
               register={register}
               error={errors.role}
+              placeholder="---"
             />
             <FormSelectField
               label="Clothing size"
               name="clothingSize"
-              register={register}
               error={errors.clothingSize}
               options={clothingSizes.map((size) => ({
                 value: size,
                 label: size,
               }))}
               setValue={setValue}
+              placeholder="---"
             />
-            <FormSelectField
+            <FormRadioField
               label="Participation type"
               name="participationType"
-              register={register}
               error={errors.participationType}
               options={participationTypes.map((type) => ({
-                value: type,
+                value: type.toLocaleLowerCase().replace(" ", "_"),
                 label: type,
               }))}
               setValue={setValue}
+              value={getValues("participationType")}
+              register={register}
             />
             {AfterPart}
+            <div className="my-4 flex flex-col">
+              <div className="flex items-center">
+                <Checkbox
+                  onCheckedChange={(value: boolean) =>
+                    setValue("personalData", value)
+                  }
+                  className="rounded-full data-[state=checked]:text-primary data-[state=checked]:bg-primary-foreground"
+                  name={"personalData"}
+                  id="personalData"
+                />
+                <Label htmlFor="personalData" className="ml-2">
+                  I agree to the processing of my personal data
+                </Label>
+              </div>
+              {errors.personalData && (
+                <p className="text-red-500">{errors.personalData.message}</p>
+              )}
+            </div>
+            <div className="my-4">
+              <Turnstile
+                id="contact-turnstile"
+                siteKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!}
+                options={{
+                  action: "Contact",
+                  theme: "dark",
+                }}
+                onSuccess={(token) => setValue("captchaToken", token)}
+              />
+              {errors.captchaToken && (
+                <p className="text-red-500">{errors.captchaToken.message}</p>
+              )}
+            </div>
             <Button type="submit">Submit</Button>
           </form>
         </DialogContent>
