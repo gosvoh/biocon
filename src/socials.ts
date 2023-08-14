c5d675e9195f8a5a2d4ac30b8288e9fd9612b901
Aleksey Vokhmin <vohmina2011@yandex.ru>
2023-08-14 16:35:49 +0300

    Fixed link

--- src/socials.ts
+++ src/socials.ts
@@ -1,5 +1,5 @@
 export const socials = {
-  vk: "https://vk.com/event221790005",
+  vk: "https://vk.com/public221897115",
   telegram: "https://web.telegram.org/k/#@BIOCON_2023",
   facebook: "https://facebook.com/biocon2023",
   youtube: "https://youtube.com/@BIOCON_2023",
