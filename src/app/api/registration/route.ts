14655d99a6e5bb795aad101f482096b7228d0a2e
Aleksey Vokhmin <vohmina2011@yandex.ru>
2023-08-15 23:35:20 +0300

    API fix

--- src/app/api/registration/route.ts
+++ src/app/api/registration/route.ts
@@ -1,101 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
-import isMobilePhone from "validator/lib/isMobilePhone";
-import isURL from "validator/lib/isURL";
-import * as yup from "yup";
 import prisma from "@/lib/prisma";
 import { checkCaptchaToken } from "../utils";
-
-const formSchema = yup.object().shape({
-  name: yup
-    .string()
-    .required("Please enter your name")
-    .min(3, "Name must be at least 3 characters long"),
-  email: yup
-    .string()
-    .required("Please enter your email address")
-    .email("Please enter a valid email address"),
-  mobile: yup
-    .string()
-    .required("Please enter your mobile number")
-    .test("is-mobile-phone", "Please enter a valid mobile number", (value) =>
-      isMobilePhone(value, "any")
-    ),
-  country: yup.string().required("Please select the country you are from"),
-  city: yup.string().required("Please enter the city you are from"),
-  affiliation: yup.string().required("Please enter your affiliation"),
-  role: yup.string().required("Please enter your role"),
-  otherRole: yup
-    .string()
-    .when("role", ([value], schema) =>
-      value === "Other" ? schema.required("Please enter your role") : schema
-    ),
-  clothingSize: yup.string().required("Please select your clothing size"),
-  participationType: yup
-    .string()
-    .required("Please select your participation type"),
-  motivationLetter: yup
-    .string()
-    .when("participationType", ([value], schema) =>
-      value === "Attendee"
-        ? schema
-            .required("Please enter a motivation letter")
-            .min(10, "Motivation letter must be at least 10 characters long")
-        : schema
-    ),
-  researchInterests: yup
-    .string()
-    .when("participationType", ([value], schema) =>
-      value !== "Attendee"
-        ? schema.required("Please enter your research interests")
-        : schema
-    ),
-  tentativeTitle: yup
-    .string()
-    .when("participationType", ([value], schema) =>
-      value === "Invited Speaker"
-        ? schema.required("Please enter a tentative title")
-        : schema
-    ),
-  resume: yup.string().when("participationType", ([value], schema) =>
-    value === "Invited Speaker"
-      ? schema
-          .required("Please enter a resume")
-          .test("is-url", "Please enter a valid URL", (value) =>
-            isURL(value, {
-              protocols: ["http", "https"],
-              require_protocol: true,
-            })
-          )
-      : schema
-  ),
-  scienceProfile: yup.string().when("participationType", ([value], schema) =>
-    value !== "Attendee"
-      ? schema
-          .required("Please enter a science profile")
-          .test("is-url", "Please enter a valid URL", (value) =>
-            isURL(value, {
-              protocols: ["http", "https"],
-              require_protocol: true,
-            })
-          )
-      : schema
-  ),
-  video: yup.string().when("participationType", ([value], schema) =>
-    value !== "Attendee"
-      ? schema
-          .required("Please enter a video")
-          .test("is-url", "Please enter a valid URL", (value) =>
-            isURL(value, {
-              protocols: ["http", "https"],
-              require_protocol: true,
-            })
-          )
-      : schema
-  ),
-});
+import { Registrations } from "@prisma/client";
 
 export async function POST(req: NextRequest) {
-  const { captchaToken, personalData, ...data } = await req.json();
+  const {
+    captchaToken,
+    personalData,
+    ...data
+  }: {
+    captchaToken: string;
+    personalData: boolean;
+    otherRole?: string;
+  } & Registrations = await req.json();
 
   if (!(await checkCaptchaToken(captchaToken)))
     return NextResponse.json(
@@ -103,8 +20,9 @@ export async function POST(req: NextRequest) {
       { status: 400 }
     );
 
+  if (data.role === "Other" && data.otherRole) data.role = data.otherRole;
+
   try {
-    await formSchema.validate(data, { abortEarly: false });
     return NextResponse.json({
       message: "Registration successful",
       registration: await prisma.registrations.create({ data }),
