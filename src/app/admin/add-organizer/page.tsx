83628dc734b27c75ce45cee8ac63d1a8eff5eba9
Aleksey Vokhmin <vohmina2011@yandex.ru>
2023-08-15 23:35:27 +0300

    CSS fix

--- src/app/admin/add-organizer/page.tsx
+++ src/app/admin/add-organizer/page.tsx
@@ -27,8 +27,7 @@ import React, { useEffect, useReducer, useState } from "react";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <div></div>
-      <div className="flex items-center justify-center p-4">
+      <div className="min-h-screen flex items-center justify-center p-4">
         <ConfigProvider theme={{ algorithm: theme.darkAlgorithm }}>
           {children}
         </ConfigProvider>
