71e8e8aa5b3a0f6178d6da34ece991c341306827
Aleksey Vokhmin <vohmina2011@yandex.ru>
2023-08-17 16:35:47 +0300

    Fixed mail msg

--- src/app/api/registration/mail.tsx
+++ src/app/api/registration/mail.tsx
@@ -2,6 +2,15 @@ export const Mail = (name: string) => {
   return (
     <>
       <h1>{name}, Thank you for registration for BIOCON 2023!</h1>
+      <img
+        src={`${
+          process.env.NODE_ENV === "development"
+            ? "http://localhost:3000"
+            : "https://biocon.international"
+        }/banner.png`}
+        alt="Biocon banner"
+        style={{ width: "100%" }}
+      />
       <p>Dear {name},</p>
       <p>
         On behalf of the Organizing Committee, we are excited to welcome you to
@@ -19,13 +28,22 @@ export const Mail = (name: string) => {
         keynote speakers, an exciting Science Slam and much more — all infused
         with unforgettable culture of Tatarstan — BIOCON, in one word.
       </p>
-      <p>We have collected some BIOCON trivia for our guests:</p>
+      <p style={{ textDecoration: "underline", fontWeight: "bold" }}>
+        We have collected some BIOCON trivia for our guests:
+      </p>
       <ul>
-        <li>The floor language is English;</li>
-        <li>No registration fee is applied to any types of participants;</li>
         <li>
-          We only offer offline participation in an attempt to bring back the
-          unique international conference vibe;
+          The floor language is{" "}
+          <span style={{ fontWeight: "bold" }}>English</span>;
+        </li>
+        <li>
+          <span style={{ fontWeight: "bold" }}>No registration fee</span> is
+          applied to any types of participants;
+        </li>
+        <li>
+          We <span style={{ fontWeight: "bold" }}>only</span> offer{" "}
+          <span style={{ fontWeight: "bold" }}>offline participation</span> in
+          an attempt to bring back the unique international conference vibe;
         </li>
         <li>
           The program will soon be available at{" "}
