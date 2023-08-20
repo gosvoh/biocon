384d2bf121184d23e433721cb0abb10d63ec637b
Aleksey Vokhmin <vohmina2011@yandex.ru>
2023-08-20 20:11:03 +0300

    Added security headers Removed legacy browsers support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
+    async headers() {
+        return [{
+            source: "/(.*)",
+            headers: [
+                { key: "X-Frame-Options", value: "DENY" },
+                { key: "X-Content-Type-Options", value: "nosniff" },
+                { key: "Referrer-Policy", value: "same-origin" },
+                { key: "X-XSS-Protection", value: "1; mode=block" },
+            ]
+        }];
+    },
+    experimental: {
+        legacyBrowsers: false
+    },
     images: {
         remotePatterns: [
             {
