7ad67c7f369b7620f9d0a2f3fca73b09ea29a7db
Aleksey Vokhmin <vohmina2011@yandex.ru>
2023-08-24 17:22:06 +0300

    Changed link to facebook

--- src/socials.ts
+++ src/socials.ts
@@ -1,6 +1,6 @@
 export const socials = {
   vk: "https://vk.com/public221897115",
   telegram: "https://web.telegram.org/k/#@BIOCON_2023",
-  facebook: "https://facebook.com/biocon2023",
+  facebook: "https://www.facebook.com/groups/biocon2023",
   youtube: "https://youtube.com/@BIOCON_2023",
 };
