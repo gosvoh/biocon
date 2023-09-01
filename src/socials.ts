28d8786e08e4b13f1ecf983980a0c08df3b67ae0
Aleksey Vokhmin <vohmina2011@yandex.ru>
2023-09-01 22:14:16 +0300

    Changed telegram link

--- src/socials.ts
+++ src/socials.ts
@@ -1,6 +1,6 @@
 export const socials = {
   vk: "https://vk.com/public221897115",
-  telegram: "https://web.telegram.org/k/#@BIOCON_2023",
+  telegram: "https://t.me/BIOCON_2023",
   facebook: "https://www.facebook.com/groups/biocon2023",
   youtube: "https://youtube.com/@BIOCON_2023",
 };
