0f525351df062136b66351f0ccd53996156495c6
gosvoh <vokhmina2011@yandex.ru>
2023-09-27 15:55:54 +0300

    Changed facebook link

--- src/socials.ts
+++ src/socials.ts
@@ -1,6 +1,6 @@
 export const socials = {
   vk: "https://vk.com/public221897115",
   telegram: "https://t.me/BIOCON_2023",
-  facebook: "https://www.facebook.com/groups/biocon2023",
+  facebook: "https://www.facebook.com/biocon.international",
   youtube: "https://youtube.com/@BIOCON_2023",
 };
