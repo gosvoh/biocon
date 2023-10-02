aead01f0d179d0de6eccfb8a81eea4bb3986e35a
Aleksey Vokhmin <vohmina2011@yandex.ru>
2023-10-03 00:44:05 +0300

    Removed Referrer-Policy header

--- next.config.js
+++ next.config.js
@@ -1,29 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone",
-    async headers() {
-        return [{
-            source: "/(.*)",
-            headers: [
-                { key: "X-Frame-Options", value: "DENY" },
-                { key: "X-Content-Type-Options", value: "nosniff" },
-                { key: "Referrer-Policy", value: "same-origin" },
-                { key: "X-XSS-Protection", value: "1; mode=block" },
-            ]
-        }];
-    },
-    experimental: {
-        legacyBrowsers: false
-    },
-    images: {
-        remotePatterns: [
-            {
-                hostname: "biocon.international",
-                pathname: "/images/**",
-                protocol: "https"
-            }
-        ]
-    }
-}
+  output: "standalone",
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "X-Frame-Options", value: "DENY" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "X-XSS-Protection", value: "1; mode=block" },
+        ],
+      },
+    ];
+  },
+  experimental: {
+    legacyBrowsers: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "biocon.international",
+        pathname: "/images/**",
+        protocol: "https",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
