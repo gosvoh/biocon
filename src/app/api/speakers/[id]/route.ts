63d6b72d4744d348199beda1689db5d0a627fede
Aleksey Vokhmin <vohmina2011@yandex.ru>
2023-11-16 00:43:51 +0300

    Remove thunder variable from speaker validation

--- src/app/api/speakers/[id]/route.ts
+++ src/app/api/speakers/[id]/route.ts
@@ -54,7 +54,6 @@ export async function PATCH(
       !university ||
       !universityUrl ||
       !hIndex ||
-      !thunder ||
       !speakerType ||
       !country
     )
--- src/app/api/speakers/route.ts
+++ src/app/api/speakers/route.ts
@@ -41,7 +41,6 @@ export async function POST(req: NextRequest) {
     !universityUrl ||
     !hIndex ||
     !img ||
-    !thunder ||
     !speakerType ||
     !country
   )
