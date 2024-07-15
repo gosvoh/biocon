0d0a836cb195675289fe1ec26aba12292c941361
Aleksey Vokhmin <vohmina2011@yandex.ru>
2024-07-16 00:40:45 +0300

    Updated prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,17 @@
-// import { PrismaClient as BioconClient } from "@prisma/client/biocon";
+import { PrismaClient as BioconClient } from ".prisma/client/biocon";
 // import { PrismaClient as WorldClient } from "@prisma/client/world";
 
-// let biocon: BioconClient;
-// let world: WorldClient;
+export let biocon: BioconClient;
+// export let world: WorldClient;
 
-// if (process.env.NODE_ENV === "production") biocon = new BioconClient();
-// else {
-//   if (!(global as any).biocon) (global as any).biocon = new BioconClient();
-//   biocon = (global as any).biocon;
-// }
+if (process.env.NODE_ENV === "production") biocon = new BioconClient();
+else {
+  if (!(global as any).biocon) (global as any).biocon = new BioconClient();
+  biocon = (global as any).biocon;
+}
 
 // if (process.env.NODE_ENV === "production") world = new WorldClient();
 // else {
 //   if (!(global as any).world) (global as any).world = new WorldClient();
 //   world = (global as any).world;
 // }
-
-// export { biocon, world };
