314cf3ee53b0758d2bf2f585de7fd755838488ee
Aleksey Vokhmin <vohmina2011@yandex.ru>
2024-07-15 13:47:26 +0300

    Build fix

--- src/app/_api/mailer.ts
+++ src/app/_api/mailer.ts
@@ -1,30 +1,30 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.timeweb.ru",
-  auth: {
-    user: process.env.MAIL_USER!,
-    pass: process.env.MAIL_PASS!,
-  },
-  port: 465,
-  secure: true,
-  dkim: {
-    domainName: "biocon.international",
-    keySelector: "mail",
-    privateKey: process.env.DKIM_PRIVATE_KEY!,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.timeweb.ru",
+//   auth: {
+//     user: process.env.MAIL_USER!,
+//     pass: process.env.MAIL_PASS!,
+//   },
+//   port: 465,
+//   secure: true,
+//   dkim: {
+//     domainName: "biocon.international",
+//     keySelector: "mail",
+//     privateKey: process.env.DKIM_PRIVATE_KEY!,
+//   },
+// });
 
-export const sendMail = async (
-  to: string,
-  subject: string,
-  text: string,
-  html: React.ReactNode,
-) => {
-  return await transporter.sendMail({
-    to,
-    subject,
-    text,
-    html: `<html><body>${html}</body></html>`,
-  });
-};
+// export const sendMail = async (
+//   to: string,
+//   subject: string,
+//   text: string,
+//   html: React.ReactNode,
+// ) => {
+//   return await transporter.sendMail({
+//     to,
+//     subject,
+//     text,
+//     html: `<html><body>${html}</body></html>`,
+//   });
+// };
