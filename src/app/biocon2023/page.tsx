d5ff5d16d7a5af534573b63927657771e93d4ff4
Nikita <146779463+Nikita1238393949@users.noreply.github.com>
2024-07-18 01:27:27 +0300

    feat: новая галерея + подгрузка спикеров с админки ( Biocon 2023 )

--- src/app/biocon2023/page.tsx
+++ src/app/biocon2023/page.tsx
@@ -1,11 +1,14 @@
 import { cn } from "@/lib/utils";
-import Image from "next/image";
+import Image, {StaticImageData} from "next/image";
 import { Architects_Daughter } from "next/font/google";
 import LogoDesktop from "@public/LogoDesktop.png";
 import LogoMobile from "@public/LogoMobile.png";
 import boom_right from "@public/boom_right.svg";
 import boom_left from "@public/boom_left.svg";
 import boom_down from "@public/boom_down.svg";
+import { Speakers2023 } from "@/db/schema";
+import { biocon } from "@/db/db";
+
 import "./noise.css";
 import {
   Carousel,
@@ -23,7 +26,14 @@ import img4 from "@public/previous_biocon/D-BUvQriEfw.jpg";
 import ButtonRegistration from "@/components/button.registration";
 import speaker1_square from "@public/previous_biocon/speaker_square.jpg";
 import speaker3_square from "@public/previous_biocon/speaker2_square.jpeg";
-import mirza_pc from "@public/previous_biocon/mirza_pc.jpeg";
+import mirzamob from "@public/previous_biocon/mirzamob.jpeg";
+
+// lower gallery
+import lower_anna_speaker from "@public/previous_biocon/ZZZ_6500.jpg";
+import lower_man_speaker from "@public/previous_biocon/ZZZ_5948.jpg";
+import lower_woman_speaker from "@public/previous_biocon/ZZZ_7525.jpg";
+import lower_man_affiliation_speaker from "@public/previous_biocon/ZZZ_9953.jpg";
+import lower_man_with_glasses_speaker from "@public/previous_biocon/ZZZ_2148.jpg";
 
 // glows
 import SideGlow from "@public/SideGlow.svg";
@@ -56,6 +66,16 @@ const LeftGlow = ({
     />
   </div>
 );
+
+const getSpeakers = async () =>{
+  let speakers: (typeof Speakers2023.$inferSelect)[] = [];
+  try {
+    speakers = await biocon.select().from(Speakers2023);
+  } catch (e) {
+    console.error(e);
+  }
+  return speakers
+}
 
 export default function PreviousPage() {
   return (
@@ -96,15 +116,15 @@ export default function PreviousPage() {
       </section>
 
       <section className={"relative flex flex-col gap-8"}>
-        <div className={"lg:flex lg:flex-row hidden relative"}>
+        <div className={"lg:flex lg:flex-col hidden relative gap-8"}>
           <Image
             src={boom_right}
             alt={"boom"}
             className="absolute -right-12 -top-24 w-16"
           />
-          <div className="grid grid-cols-[2fr_1fr] gap-6">
-            <div className="grid grid-rows-2 gap-6">
-              <div className="grid grid-cols-[2fr_1fr] gap-6">
+          <div className="grid grid-cols-[2fr_0.8fr] gap-8">
+            <div className="grid grid-rows-2 gap-8">
+              <div className="grid grid-cols-[1.5fr_1fr] gap-8">
                 <div className="relative flex items-center justify-center h-full">
                   <Image
                     className="rounded-[28px] object-cover w-full h-full"
@@ -112,7 +132,7 @@ export default function PreviousPage() {
                     alt="GL"
                   />
                 </div>
-                <div className="relative flex items-center justify-center h-full">
+                <div className="relative flex items-center justify-center">
                   <Image
                     className="rounded-[28px] object-cover w-full h-full aspect-square"
                     src={speaker1_square}
@@ -120,10 +140,10 @@ export default function PreviousPage() {
                   />
                 </div>
               </div>
-              <div className="grid grid-cols-[1fr_2fr] gap-6">
+              <div className="grid grid-cols-[1fr_1.5fr] gap-8">
                 <div className="relative flex items-center justify-center h-full">
                   <Image
-                    className="rounded-[28px] object-cover w-full h-full"
+                    className="rounded-[28px] object-cover w-full h-full aspect-square"
                     src={speaker3_square}
                     alt="GL"
                   />
@@ -140,15 +160,58 @@ export default function PreviousPage() {
             <div className="relative flex items-stretch h-full">
               <Image
                 className="rounded-[28px] object-cover w-full h-full"
-                src={mirza_pc}
+                src={speaker2}
                 alt="GL"
               />
+            </div>
+          </div>
+          <div className="grid grid-cols-[0.775fr_2fr] gap-8">
+            <div className="relative flex items-stretch h-50%">
+              <Image
+                className="rounded-[28px] object-cover w-full h-50% scale-1.3"
+                src={lower_anna_speaker}
+                alt="GL"
+              />
+            </div>
+            <div className="grid grid-rows-2 gap-8">
+              <div className="grid grid-cols-[1fr_1.5fr] gap-8">
+                <div className="relative flex items-center justify-center">
+                  <Image
+                    className="rounded-[28px] object-cover w-full h-full aspect-square"
+                    src={lower_man_speaker}
+                    alt="GL"
+                  />
+                </div>
+                <div className="relative flex items-center justify-center h-full">
+                  <Image
+                    className="rounded-[28px] object-cover w-full h-full"
+                    src={lower_woman_speaker}
+                    alt="GL"
+                  />
+                </div>
+              </div>
+              <div className="grid grid-cols-[1.5fr_1fr] gap-8">
+                <div className="relative flex items-center justify-center h-full">
+                  <Image
+                    className="rounded-[28px] object-cover w-full h-full"
+                    src={lower_man_affiliation_speaker}
+                    alt="GL"
+                  />
+                </div>
+                <div className="relative flex items-center justify-center h-full">
+                  <Image
+                    className="rounded-[28px] object-cover w-full h-full aspect-w-1 aspect-h-1"
+                    src={lower_man_with_glasses_speaker}
+                    alt="GL"
+                  />
+                </div>
+              </div>
             </div>
           </div>
           <Image
             src={boom_down}
             alt={"boom"}
-            className="absolute -left-12 -bottom-[4.5rem] w-16"
+            className="absolute -left-12 -bottom-[6rem] w-16"
           />
         </div>
         <div className={"relative flex flex-col gap-4 lg:hidden"}>
@@ -167,7 +230,7 @@ export default function PreviousPage() {
           <Image className={"rounded-[16px]"} src={img1} alt={"GL"} />
           <div className={"grid grid-cols-2 gap-3"}>
             <Image className={"rounded-[16px]"} src={speaker1} alt={"GL"} />
-            <Image className={"rounded-[16px]"} src={speaker2} alt={"GL"} />
+            <Image className={"rounded-[16px]"} src={mirzamob} alt={"GL"} />
           </div>
           <Image className={"rounded-[16px]"} src={img4} alt={"GL"} />
         </div>
@@ -240,12 +303,15 @@ export default function PreviousPage() {
         </div>
       </section>
 
-      <section className="relative flex flex-col gap-1">
+      <section className="relative flex flex-col gap-3 lg:gap-6">
         <h2 className="font-normal m-0">BIOCON’23 speakers</h2>
-        <p className="font-light mb-10 text-lg lg:text-xl">Plenary speakers</p>
+        <h3 className="font-normal lg:mb-6 mb-2">Plenary speakers</h3>
         <div className="hidden grid-cols-4 gap-14 lg:grid">
           {plenary_speakers.map((speaker, index) => (
-            <div key={index} className="grid grid-rows-[1fr,0.5fr] gap-5">
+            <div
+              key={index}
+              className="grid grid-rows-[1fr,0.5fr] xl:grid-rows-[1fr,0.3fr] gap-5"
+            >
               <Image
                 className="rounded-[28px] h-full object-cover aspect-square"
                 src={speaker.src}
@@ -263,38 +329,40 @@ export default function PreviousPage() {
         <div className={"lg:hidden"}>
           <Carousel>
             <CarouselContent>
-              {plenary_speakers.map((speaker, index) => (
-                <CarouselItem
-                  key={index}
-                  className={"md:basis-1/2 xl:basis-1/3"}
-                >
-                  <div key={index} className="grid grid-rows-[1fr,0.4fr] gap-5">
-                    <Image
-                      className="rounded-[28px] h-full object-cover aspect-square"
-                      src={speaker.src}
-                      alt=""
-                    />
-                    <div>
-                      <p className={"text-center"}>{speaker.name}</p>
-                      <p className={"font-light lg:text-lg text-center"}>
-                        {speaker.university}
-                      </p>
+              {getSpeakers().then((speakers)=>speakers.map((speaker, index) => (
+                  <CarouselItem
+                      key={index}
+                      className={"md:basis-1/2 xl:basis-1/3"}
+                  >
+                    <div key={index} className="grid grid-rows-[1fr,0.4fr] gap-5">
+                      <Image
+                          className="rounded-[28px] h-full object-cover aspect-square"
+                          src={"/" + speaker.image}
+                          alt="Description of the image"
+                          layout="fill"
+                          objectFit="cover"
+                      />
+                      <div>
+                        <p className={"text-center"}>{speaker.name}</p>
+                        <p className={"font-light lg:text-lg text-center"}>
+                          {speaker.university}
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                </CarouselItem>
-              ))}
+                  </CarouselItem>
+              )))}
             </CarouselContent>
             <CarouselPrevious />
             <CarouselNext />
           </Carousel>
         </div>
       </section>
-
+      <LeftGlow className={"ml-20 w-[150%] h-fit"} />
       <LeftGlow big className={"lg:block hidden"} />
       <Image
         src={BottomGlow}
         alt=""
-        className="absolute w-fit md:w-[125%] md:h-fit max-w-none -translate-x-1/2 -top-20 left-1/2 -z-10 rotate-180"
+        className="absolute w-fit md:w-fit md:h-fit max-w-none -translate-x-1/2 -top-20 left-1/2 -z-10 rotate-180"
       />
       <Image
         src={BottomGlow}
