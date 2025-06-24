7fcca5560a315f0a86659a622bba6a07f0d17d7f
Aleksey Vokhmin <vohmina2011@yandex.ru>
2025-06-24 14:19:09 +0300

    refactor: streamline EditorJS component and dynamically load tools to fix Element is not defined error

--- src/components/EditorJS.tsx
+++ src/components/EditorJS.tsx
@@ -1,66 +1,17 @@
 "use client";
-import EditorJs from "@natterstefan/react-editor-js";
+
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 import type EditorJSType from "@editorjs/editorjs";
 import type { ToolConstructable, ToolSettings } from "@editorjs/editorjs";
-//@ts-expect-error 7016
-import Table from "@editorjs/table";
-// import List from '@editorjs/list'
-//@ts-expect-error 7016
-import Warning from "@editorjs/warning";
-//@ts-expect-error 7016
-import LinkTool from "@editorjs/link";
-// import Image from '@editorjs/image';
-//@ts-expect-error 7016
-import Raw from "@editorjs/raw";
-import Header from "@editorjs/header";
-//@ts-expect-error 7016
-import Quote from "@editorjs/quote";
-//@ts-expect-error 7016
-import Marker from "@editorjs/marker";
-//@ts-expect-error 7016
-import CheckList from "@editorjs/checklist";
-//@ts-expect-error 7016
-import Delimiter from "@editorjs/delimiter";
-//@ts-expect-error 7016
-import SimpleImage from "@editorjs/simple-image";
-// import Paragraph from 'editorjs-paragraph-with-alignment';
-import NestedList from "@editorjs/nested-list";
-import styled from "styled-components";
+import { Skeleton } from "./ui/skeleton";
+import { cn } from "@/lib/utils";
 
 const Container = styled.div`
   .cdx-marker {
     color: inherit;
   }
 `;
-
-const EDITOR_JS_TOOLS = {
-  table: Table,
-  marker: Marker,
-  list: NestedList,
-  warning: Warning,
-  linkTool: LinkTool,
-  // image: Image,
-  raw: Raw,
-  //   paragraph: Paragraph,
-  header: {
-    class: Header,
-    config: {
-      placeholder: "Enter a header",
-      levels: [1, 2, 3, 4, 5, 6],
-      defaultLevel: 3,
-    },
-  },
-  //   attaches: {
-  //     class: AttachesTool,
-  //     config: {
-  //       //   endpoint: 'http://localhost:8008/uploadFile'
-  //     },
-  //   },
-  quote: Quote,
-  checklist: CheckList,
-  delimiter: Delimiter,
-  image: SimpleImage,
-};
 
 export const EditorJSElement = ({
   viewOnly = false,
@@ -75,22 +26,110 @@ export const EditorJSElement = ({
   onEditorInstance?: ((instance: EditorJSType) => void) | undefined;
   className?: string;
 }) => {
+  const [isClient, setIsClient] = useState(false);
+  const [EditorJs, setEditorJs] = useState<any>(null);
+  const [tools, setTools] = useState<any>(null);
+
+  useEffect(() => {
+    setIsClient(true);
+
+    // Динамически импортируем все EditorJS компоненты только на клиенте
+    const loadEditorComponents = async () => {
+      const [
+        { default: EditorJsComponent },
+        { default: Table },
+        { default: Warning },
+        { default: LinkTool },
+        { default: Raw },
+        { default: Header },
+        { default: Quote },
+        { default: Marker },
+        { default: CheckList },
+        { default: Delimiter },
+        { default: SimpleImage },
+        { default: NestedList },
+      ] = await Promise.all([
+        import("@natterstefan/react-editor-js"),
+        // @ts-expect-error 7016
+        import("@editorjs/table"),
+        // @ts-expect-error 7016
+        import("@editorjs/warning"),
+        // @ts-expect-error 7016
+        import("@editorjs/link"),
+        // @ts-expect-error 7016
+        import("@editorjs/raw"),
+        import("@editorjs/header"),
+        // @ts-expect-error 7016
+        import("@editorjs/quote"),
+        // @ts-expect-error 7016
+        import("@editorjs/marker"),
+        // @ts-expect-error 7016
+        import("@editorjs/checklist"),
+        // @ts-expect-error 7016
+        import("@editorjs/delimiter"),
+        // @ts-expect-error 7016
+        import("@editorjs/simple-image"),
+        import("@editorjs/nested-list"),
+      ]);
+
+      const EDITOR_JS_TOOLS = {
+        table: Table,
+        marker: Marker,
+        list: NestedList,
+        warning: Warning,
+        linkTool: LinkTool,
+        raw: Raw,
+        header: {
+          class: Header as unknown as ToolConstructable,
+          config: {
+            placeholder: "Enter a header",
+            levels: [1, 2, 3, 4, 5, 6],
+            defaultLevel: 3,
+          },
+        },
+        quote: Quote,
+        checklist: CheckList,
+        delimiter: Delimiter,
+        image: SimpleImage,
+      } satisfies {
+        [toolName: string]: ToolConstructable | ToolSettings;
+      };
+
+      setEditorJs(EditorJsComponent);
+      setTools(EDITOR_JS_TOOLS);
+    };
+
+    loadEditorComponents();
+  }, []);
+
+  if (!isClient || !EditorJs || !tools)
+    return (
+      <Container
+        className={cn(
+          "flex flex-col gap-2 max-w-[650px] w-full mx-auto",
+          className,
+        )}
+      >
+        <Skeleton className="h-32 w-full" />
+        {Array.from({ length: 10 }).map((_, index) => (
+          <Skeleton
+            key={index}
+            className={`h-12 w-${Math.floor(Math.random() * 5) + 8}/12`}
+          />
+        ))}
+      </Container>
+    );
+
   return (
     <Container className={className}>
       <EditorJs
         readOnly={viewOnly}
-        tools={
-          EDITOR_JS_TOOLS as unknown as {
-            [toolName: string]: ToolConstructable | ToolSettings;
-          }
-        }
+        tools={tools}
         data={JSON.parse(initialData)}
         holder="editorJS"
         onReady={onReady}
         editorInstance={onEditorInstance}
-      >
-        <div id="editorJS" />
-      </EditorJs>
+      />
     </Container>
   );
 };
